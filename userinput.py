5f06cec815c7aa14486d49a08479b0635942a972
rohannilatkar <iix.preyliet@gmail.com>
2026-03-31 23:59:57 +0530

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -10,3 +10,29 @@ age = int(age)
 # Printing output
 print("Hello", name)
 print("You will be", age + 5
+
+      # Taking numbers from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Taking operation
+operation = input("Enter operation (+, -, *, /): ")
+
+# Performing calculation
+if operation == "+":
+    print("Result:", num1 + num2)
+
+elif operation == "-":
+    print("Result:", num1 - num2)
+
+elif operation == "*":
+    print("Result:", num1 * num2)
+
+elif operation == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero")
+
+else:
+    print("Invalid operation")
