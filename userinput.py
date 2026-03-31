951f89026a918aff8fa3311b4e3b5cf7a183d6a4
rohannilatkar <iix.preyliet@gmail.com>
2026-03-31 23:59:29 +0530

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,12 @@
+# Taking user input for name (string)
+name = input("Enter your name: ")
+
+# Taking user input for age (string initially)
+age = input("Enter your age: ")
+
+# Converting age from string to integer
+age = int(age)
+
+# Printing output
+print("Hello", name)
+print("You will be", age + 5
